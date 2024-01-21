eb29dee5dc38338281eb83b5c44ddded0a82c42b
Chris Doyle <cdoyle@smartergridsolutions.com>
2024-01-21 12:55:53 +0000

    Issue #19 Adds tests for the core module

--- tests/core/test_core.py
+++ tests/core/test_core.py
@@ -7,6 +7,8 @@ from terragraph.core import HighlightingMode, Terragraph
 
 GRAPH_STRING_EDGES = 25
 GRAPH_STRING_NODES = 7
+NODE1_NAME = '"[root] module.mod1.random_pet.this2 (expand)"'
+NODE2_NAME = '"[root] module.mod2.random_pet.this (expand)"'
 
 
 def test_get_edges(terragraph_example):
@@ -27,7 +29,7 @@ def test_get_nodes(terragraph_example):
     assert len(terragraph_example.get_nodes()) == GRAPH_STRING_NODES
 
 
-def test_get_highlighted_nodes(terragraph_example):
+def test_highlighted_node(terragraph_example):
     """
     Passes a node to be highlighted and checks its highlighted nodes
     :param terragraph_example:
@@ -38,27 +40,45 @@ def test_get_highlighted_nodes(terragraph_example):
 
 
 @pytest.mark.parametrize(
-    ("node", "mode", "num_edges"),
+    ("node_name", "highlight_mode", "expected_num_edges"),
     [
         (
-            '"[root] module.mod1.random_pet.this2 (expand)"',
+            NODE1_NAME,
             HighlightingMode.SUCCESSOR,
             4,
         ),
         (
-            '"[root] module.mod1.random_pet.this2 (expand)"',
+            NODE1_NAME,
             HighlightingMode.PRECEDING,
             7,
         ),
-        ('"[root] module.mod1.random_pet.this2 (expand)"', HighlightingMode.ALL, 11),
+        (NODE1_NAME, HighlightingMode.ALL, 11),
+        (
+            NODE2_NAME,
+            HighlightingMode.SUCCESSOR,
+            4,
+        ),
+        (
+            NODE2_NAME,
+            HighlightingMode.PRECEDING,
+            8,
+        ),
+        (NODE2_NAME, HighlightingMode.ALL, 12),
     ],
 )
-def test_get_highlighted_edges(node, mode, num_edges, graph_string):
+def test_highlight_node_edges(
+    node_name, highlight_mode, expected_num_edges, graph_string
+):
     """
-    Passes a node to be highlighted and checks its highlighted edges
-    :param terragraph_example:
+    Will use a node it a Highlighting mode and test that the expected number of edges were highlighted
+    :param node_name: The node name to highlight
+    :param highlight_mode: The Highlighting mode
+    :param expected_num_edges: The expected number of edges
+    :param graph_string: The graph_string example
     :return:
     """
-    terragraph_example = Terragraph(dot_data=graph_string, highlighting_mode=mode)
-    terragraph_example.highlight_node(node)
-    assert len(terragraph_example.get_highlighted_edges()) == num_edges
+    terragraph_example = Terragraph(
+        dot_data=graph_string, highlighting_mode=highlight_mode
+    )
+    terragraph_example.highlight_node_edges(node_name)
+    assert len(terragraph_example.get_highlighted_edges()) == expected_num_edges
